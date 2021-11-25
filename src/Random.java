6477c38c55a0a06341bef3bbdee8f05785c16745
aphil311 <aidanphillips21@outlook.com>
2021-11-24 23:34:41 -0500

    added random opponent

---
+++ src/Random.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class Random extends Player {
+    public Random(int val, Board board, Color color) {
+        super(val, board, color);
+    }
+
+    /**
+     * randomly chooses a column to play
+     */
+    public void move() {
+        int column;
+        do {
+            column = StdRandom.uniform(board.getCols());
+        } while (board.isFull(column));
+        board.insert(column, val);
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        Player random = new Random(1, board, Color.RED);
+        Player human = new Human(2, board, Color.YELLOW);
+        // a few moves to make sure everything works fine
+        for (int i = 0; i < 7; i++) {
+            random.move();
+            StdOut.println(board);
+            human.move();
+        }
+    }
+}
