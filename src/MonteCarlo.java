972a95adb93273b10dcda2a519db7dfc2a0d22f3
aphil311 <aidanphillips21@outlook.com>
2021-11-26 11:58:44 -0500

    Create MonteCarlo.java

---
+++ src/MonteCarlo.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+public class MonteCarlo extends Player {
+    private int opp;
+
+    public MonteCarlo(int val, Board board, Color color, int opp) {
+        super(val, board, color);
+        this.opp = opp;
+    }
+
+    public void move() {
+        // for certain number of iterations
+
+        // create nodes
+
+        // calculate exploration/exploitation
+
+        // choose node
+
+        // if unexplored, roll out
+
+        // if explored, expand nodes
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
